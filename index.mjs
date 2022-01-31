6ccad795e2811a855517ca025ae5708241882656
Francisco Silvério <franciscogsilverio@gmail.com>
2022-01-31 15:56:28 -0300

    Added the 'getLinks' function

--- index.mjs
+++ index.mjs
@@ -1,6 +1,16 @@
 import chalk from "chalk";
 import fs from "fs";
 
+function getLinks(text) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const resultsArr = [];
+  let temp;
+  while ((temp = regex.exec(text)) !== null) {
+    resultsArr.push({ [temp[1]]: temp[2] });
+  }
+  return resultsArr;
+}
+
 function errorHandle(error) {
   throw new Error(chalk.red(error.code, "no file in path"));
 }
@@ -9,9 +19,11 @@ async function getFile(filePath) {
   const encoding = "utf-8";
   try {
     const text = await fs.promises.readFile(filePath, encoding);
-    console.log(chalk.green(text));
+    console.log(getLinks(text));
   } catch (error) {
     errorHandle(error);
+  } finally {
+    console.log(chalk.blue("Operation finished"));
   }
 }
 
